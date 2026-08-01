b0809eb1776656268a07768035a3cf0e3d2cf6ca
Atomjoy <mluk123@proton.me>
2026-08-01 13:54:50 +0200

    Cookie Buzz

--- resources/views/banner/actions.blade.php
+++ resources/views/banner/actions.blade.php
@@ -5,45 +5,6 @@
     'pixelId' => null, // Set Pixel Id
 ])
 
-@if($gtagId)
-<!-- INIT GOOGLE TAG (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ $gtagId }}"></script>
-<script>
-    window.dataLayer = window.dataLayer || []
-    function gtag() {
-        dataLayer.push(arguments)
-    }
-
-    gtag('js', new Date())
-    gtag('config', '{{ $gtagId }}')
-    gtag('consent', 'default', {
-        ad_storage: 'denied',
-        ad_user_data: 'denied',
-        ad_personalization: 'denied',
-        analytics_storage: 'denied',
-		wait_for_update: 1000
-    })
-</script>
-@endif
-
-@if($pixelId)
-<!-- Facebook Pixel Code -->
-<script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-
-    fbq('consent', 'revoke');
-    fbq('init', '{{ $pixelId }}');
-    fbq('track', 'PageView');
-</script>
-@endif
-
 <script>
 function checkFacebookPixel() {
 	if (window.fbq && window.fbq.getState) {
@@ -126,3 +87,42 @@ function rejectMarketing() {
 	}
 }
 </script>
+
+@if($gtagId)
+<!-- INIT GOOGLE TAG (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ $gtagId }}"></script>
+<script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+        dataLayer.push(arguments)
+    }
+
+    gtag('js', new Date())
+    gtag('config', '{{ $gtagId }}')
+    gtag('consent', 'default', {
+        ad_storage: 'denied',
+        ad_user_data: 'denied',
+        ad_personalization: 'denied',
+        analytics_storage: 'denied',
+		wait_for_update: 1000
+    })
+</script>
+@endif
+
+@if($pixelId)
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('consent', 'revoke');
+    fbq('init', '{{ $pixelId }}');
+    fbq('track', 'PageView');
+</script>
+@endif
--- resources/views/banner/gtm.blade.php
+++ resources/views/banner/gtm.blade.php
@@ -5,30 +5,6 @@
 	'gtmId' => null, // GTM-XXXXXXX
 ])
 
-@if($gtmId)
-<!-- Definition of the gtag() function and default consents (Consent Mode v2) -->
-<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){window.dataLayer.push(arguments);}
-
-	// Required by law: Block tracking by default before user's decision
-	gtag('consent', 'default', {
-		ad_storage: 'denied',
-		analytics_storage: 'denied',
-		ad_user_data: 'denied',
-		ad_personalization: 'denied',
-		wait_for_update: 1000
-	});
-</script>
-
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ $gtmId }}');</script>
-<!-- End Google Tag Manager -->
-
 <script>
 // Actions list
 const actionsList = {
@@ -111,4 +87,28 @@ function rejectMarketing() {
 	}
 }
 </script>
+
+<!-- Definition of the gtag() function and default consents (Consent Mode v2) -->
+<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){window.dataLayer.push(arguments);}
+
+	// Required by law: Block tracking by default before user's decision
+	gtag('consent', 'default', {
+		ad_storage: 'denied',
+		analytics_storage: 'denied',
+		ad_user_data: 'denied',
+		ad_personalization: 'denied',
+		wait_for_update: 1000
+	});
+</script>
+
+@if($gtmId)
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','{{ $gtmId }}');</script>
+<!-- End Google Tag Manager -->
 @endif
